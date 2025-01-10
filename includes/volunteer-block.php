033079ebad4bcdedcd97ea5c6927a6f42e4d79fb
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2025-01-10 10:56:24 +0100

    added the rest of volunteers

--- includes/volunteer-block.php
+++ includes/volunteer-block.php
@@ -2,7 +2,7 @@
                 <div class="w-layout-blockcontainer container w-container">
                     <div class="section-title">
                         <div class="sub-title">
-                            <img src="assets/sub-title.svg" loading="lazy" alt="Icon"/>
+                            <img src="/assets/sub-title.svg" loading="lazy" alt="Icon"/>
                             <div>Meet Our Team</div>
                         </div>
                         <h2 class="title">Our Volunteers</h2>
@@ -13,10 +13,10 @@
                                 <div data-w-id="9c549b85-18ac-8820-de10-57e3c00524fc" class="volunteers-block">
                                     <div class="volunteer-top">
                                         <div class="volunteer-info">
-                                            <img src="assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
+                                            <img src="/assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
                                         </div>
                                         <a href="volunteers/vinolia-apakoh" class="volunteers-img w-inline-block">
-                                            <img src="assets/vinolia-apakoh.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="assets/vinolia-apakoh.png 500w, assets/vinolia-apakoh.png 612w" class="volunteers-image"/>
+                                            <img src="/assets/vinolia-apakoh.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="/assets/vinolia-apakoh.png 500w, /assets/vinolia-apakoh.png 612w" class="volunteers-image"/>
                                         </a>
                                     </div>
                                     <a href="volunteers/vinolia-apakoh" class="volunteers-data w-inline-block">
@@ -29,10 +29,10 @@
                                 <div data-w-id="9c549b85-18ac-8820-de10-57e3c00524fc" class="volunteers-block">
                                     <div class="volunteer-top">
                                         <div class="volunteer-info">
-                                            <img src="assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
+                                            <img src="/assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
                                         </div>
                                         <a href="/volunteers/muna-de-ciman" class="volunteers-img w-inline-block">
-                                            <img src="assets/vinolia-apakoh.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="https://cdn.prod.website-files.com/66236bb750d12e7540854b5f/66265cbcf69a7cfa85e1a3c2_volunteers-thumb-04-p-500.jpg 500w, https://cdn.prod.website-files.com/66236bb750d12e7540854b5f/66265cbcf69a7cfa85e1a3c2_volunteers-thumb-04.jpg 612w" class="volunteers-image"/>
+                                            <img src="/assets/muna-de-ciman.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="/assets/muna-de-ciman.png 500w, /assets/muna-de-ciman.png 612w" class="volunteers-image"/>
                                         </a>
                                     </div>
                                     <a href="/volunteers/muna-de-ciman" class="volunteers-data w-inline-block">
@@ -45,7 +45,7 @@
                                 <div data-w-id="9c549b85-18ac-8820-de10-57e3c00524fc" class="volunteers-block">
                                     <div class="volunteer-top">
                                         <a href="/volunteers/eugene-apakoh" class="volunteers-img w-inline-block">
-                                            <img src="assets/eugene-apakoh.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="assets/eugene-apakoh.png 500w, assets/eugene-apakoh.png 612w" class="volunteers-image"/>
+                                            <img src="/assets/eugene-apakoh.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="/assets/eugene-apakoh.png 500w, /assets/eugene-apakoh.png 612w" class="volunteers-image"/>
                                         </a>
                                     </div>
                                     <a href="/volunteers/eugene-apakoh" class="volunteers-data w-inline-block">
@@ -58,10 +58,10 @@
                                 <div data-w-id="9c549b85-18ac-8820-de10-57e3c00524fc" class="volunteers-block">
                                     <div class="volunteer-top">
                                         <div class="volunteer-info">
-                                            <img src="assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
+                                            <img src="/assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
                                         </div>
                                         <a href="/volunteers/timibra-david" class="volunteers-img w-inline-block">
-                                            <img src="https://cdn.prod.website-files.com/66236bb750d12e7540854b5f/66265c9af58c59c448ddd5d6_volunteers-thumb-03.jpg" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="https://cdn.prod.website-files.com/66236bb750d12e7540854b5f/66265c9af58c59c448ddd5d6_volunteers-thumb-03-p-500.jpg 500w, https://cdn.prod.website-files.com/66236bb750d12e7540854b5f/66265c9af58c59c448ddd5d6_volunteers-thumb-03.jpg 612w" class="volunteers-image"/>
+                                        <img src="/assets/timibra-david.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="/assets/timibra-david.png 500w, /assets/timibra-david.png 612w" class="volunteers-image"/>
                                         </a>
                                     </div>
                                     <a href="/volunteers/timibra-david" class="volunteers-data w-inline-block">
@@ -75,10 +75,10 @@
                                 <div data-w-id="9c549b85-18ac-8820-de10-57e3c00524fc" class="volunteers-block">
                                     <div class="volunteer-top">
                                         <div class="volunteer-info">
-                                            <img src="assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
+                                            <img src="/assets/ic-plus.svg" loading="lazy" data-w-id="9c549b85-18ac-8820-de10-57e3c0052508" alt="Plus Icon" class="plus-icon"/>
                                         </div>
                                         <a href="/volunteers/rodlphe-adou" class="volunteers-img w-inline-block">
-                                            <img src="assets/rodolphe-adou.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="assets/rodolphe-adou.png 500w, assets/rodolphe-adou.png 612w" class="volunteers-image"/>
+                                            <img src="/assets/rodolphe-adou.png" loading="lazy" alt="Volunteers Image" sizes="(max-width: 479px) 97vw, (max-width: 991px) 30vw, 23vw" srcset="/assets/rodolphe-adou.png 500w, /assets/rodolphe-adou.png 612w" class="volunteers-image"/>
                                         </a>
                                     </div>
                                     <a href="/volunteers/rodlphe-adou" class="volunteers-data w-inline-block">

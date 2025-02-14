7853c973e7c09a0326ea405c8ae41a4b6070aa87
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2025-02-14 18:33:15 +0100

    modified volunteers list

--- volunteers/list.php
+++ volunteers/list.php
@@ -10,15 +10,15 @@ $team = [
     "muna-de-ciman" => [
         "name" => "Muna De Ciman",
         "role" => "Senior Community Outreach Advisor",
-        "bio" => "Vinolia Apakoh is the CEO and Founder of BlackCellence Network. She is a trailblazer, a passionate community champion who strongly believes in supporting and contributing to the Saskatchewan community. At an early age she learnt the concept of community involvement, a mindset and inspiration instilled in her from her parents about giving back to society.",
-        "email" => "info@blackcellencenetwork.com",
+        "bio" => "Muna Deciman and her family moved to Regina in 1994. From the moment she arrived, She began volunteering. I am propelled by a deep belief in serving others and giving back to my community. I have upheld the inspiring belief through the years as demonstrated by my relentless contributions and support to many organizations, events and worthy causes across Saskatchewan.",
+        "email" => "memunatou.d@sasktel.net",
         "image" => "/assets/muna-de-ciman.png",
     ],
     "eugene-apakoh" => [
         "name" => "Eugene Apakoh",
-        "role" => "Chief Finance Officer",
-        "bio" => "Vinolia Apakoh is the CEO and Founder of BlackCellence Network. She is a trailblazer, a passionate community champion who strongly believes in supporting and contributing to the Saskatchewan community. At an early age she learnt the concept of community involvement, a mindset and inspiration instilled in her from her parents about giving back to society.",
-        "email" => "info@blackcellencenetwork.com",
+        "role" => "Finance Director",
+        "bio" => "Eugene Apakoh obtained his bachelor’s and master’s degrees in the UK. He was a lecturer for a period of 12 years at the University of East London in the UK where he taught Accounting, Finance and Information System. He has vast experience and knowledge in Financial Reporting and Analysis.",
+        "email" => "finance@blackcellencenetwork.com",
         "image" => "/assets/eugene-apakoh.png",
     ],
     "timibra-david" => [
@@ -28,13 +28,13 @@ $team = [
         <p>Timibra David is a dynamic event manager, photographer, and project strategist dedicated to driving business growth and innovation. With experience working alongside leading architectural and creative industries, he has demonstrated expertise in media production, photography, and strategic planning.</p>
         <p>Timibra's proficiency in event management ensures flawless execution of high-impact events, while his strategic insights elevate brand visibility and contribute to long-term business success.</p>
         <p>As the Event Manager for Blackcellence Network, he plays a pivotal role in conceptualizing and delivering influential projects and events that make a lasting impact. </br>A committed advocate for community service, Timibra actively volunteers and contributes to projects aimed at fostering societal growth and development.</p>",
-        "email" => "info@blackcellencenetwork.com",
+        "email" => "events@blackcellencenetwork.com",
         "image" => "/assets/timibra-david.png",
     ],
     "rodlphe-adou" => [
         "name" => "Rodolphe Adou",
-        "role" => "Director of Communications",
-        "bio" => "Vinolia Apakoh is the CEO and Founder of BlackCellence Network. She is a trailblazer, a passionate community champion who strongly believes in supporting and contributing to the Saskatchewan community. At an early age she learnt the concept of community involvement, a mindset and inspiration instilled in her from her parents about giving back to society.",
+        "role" => "Communications Director",
+        "bio" => "Rodolphe is an Administrative Professional and Team Player with a passion for Communication, Relationship Building and Learning. It is his core belief that our journey of apprenticeship and education never stops and the vehicle that moves these virtues is the relationships we have and how well we communicate our goals and objectives with each other.",
         "email" => "info@blackcellencenetwork.com",
         "image" => "/assets/rodolphe-adou.png",
     ],

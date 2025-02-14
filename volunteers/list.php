8ac71bbb1ceca99e744ae6f79052c8cc38eb527d
Lukman Abisoye <25963466+luqmanAA@users.noreply.github.com>
2025-02-14 18:26:40 +0100

    modified volunteers list

--- volunteers/list.php
+++ volunteers/list.php
@@ -24,7 +24,10 @@ $team = [
     "timibra-david" => [
         "name" => "Timibra David",
         "role" => "Event Director",
-        "bio" => "Vinolia Apakoh is the CEO and Founder of BlackCellence Network. She is a trailblazer, a passionate community champion who strongly believes in supporting and contributing to the Saskatchewan community. At an early age she learnt the concept of community involvement, a mindset and inspiration instilled in her from her parents about giving back to society.",
+        "bio" => "
+        <p>Timibra David is a dynamic event manager, photographer, and project strategist dedicated to driving business growth and innovation. With experience working alongside leading architectural and creative industries, he has demonstrated expertise in media production, photography, and strategic planning.</p>
+        <p>Timibra's proficiency in event management ensures flawless execution of high-impact events, while his strategic insights elevate brand visibility and contribute to long-term business success.</p>
+        <p>As the Event Manager for Blackcellence Network, he plays a pivotal role in conceptualizing and delivering influential projects and events that make a lasting impact. </br>A committed advocate for community service, Timibra actively volunteers and contributes to projects aimed at fostering societal growth and development.</p>",
         "email" => "info@blackcellencenetwork.com",
         "image" => "/assets/timibra-david.png",
     ],
